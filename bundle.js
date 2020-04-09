037c9b6ef593097a6fdcf1d78acfcb0ec53c461e
Scott Ha <123viet123@gmail.com>
2020-04-08 19:25:19 -0700

    Added a dropdown and dropdown navigation

--- bundle.js
+++ bundle.js
@@ -93,61 +93,46 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
-// Scroll to top button
 var topBtn = document.getElementById('top-btn');
+var navBarName = document.getElementById('navbar-name');
 topBtn.addEventListener('click', function (e) {
   $('html,body').animate({
     scrollTop: 0
   }, 'slow');
-}); // Change title header in the nav bar to the section
-
-var navbarHeader = document.getElementById('navbar-header'); // const sections = [];
-// const main = document.getElementById('main');
-// const education = document.getElementById('education');
-// const skills = document.getElementById('skills');
-// const project = document.getElementById('project');
-// sections.push(document.getElementById('main'));
-// sections.push(document.getElementById('education'));
-// sections.push(document.getElementById('skills'));
-// sections.push(document.getElementById('projects'));
-// sections.forEach(section => {
-//   section.addEventListener('mouseover', e => {
-//     console.log(e.target === section);
-//     // section.style.backgroundColor = 'black';
-//   });
-//   section.addEventListener('mouseleave', e => {
-//     // section.style.backgroundColor = 'white';
-//   })
-// });
-
-var projects = document.querySelectorAll('.project');
-projects.forEach(function (project) {// project.addEventListener('mouseover', e => {
-  //   project.style.backgroundColor = 'black';
-  //   project.style.color = 'white';
-  // });
-  // project.addEventListener('mouseleave', e => {
-  //   project.style.backgroundColor = 'white';
-  //   project.style.color = 'black';
-  // });
 });
 
 function scroll() {
-  topBtn.style.display = document.body.scrollTop > 250 || document.documentElement.scrollTop > 250 ? "block" : "none"; // if (document.documentElement.scrollTop < 141) {
-  //   navbarHeader.textContent = "Welcome";
-  // } else if (document.documentElement.scrollTop >= 141 && document.documentElement.scrollTop < 315)  {
-  //   navbarHeader.textContent = "Education";
-  // } else if (document.documentElement.scrollTop >= 315 && document.documentElement.scrollTop < 700)  {
-  //   navbarHeader.textContent = "Skills";
-  // }
-  //   console.log(document.documentElement.scrollTop);
-  // sections.forEach(s => {
-  // })
+  if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+    topBtn.style.display = "block";
+    navBarName.style.opacity = 1;
+  } else {
+    topBtn.style.display = "none";
+    navBarName.style.opacity = 0;
+  }
 }
 
 window.onscroll = function () {
   return scroll();
-}; // document.getElementById('skills').scrollIntoView();
-// const
+}; // const repostiBg = document.querySelector('.p1bg');
+
+
+var bars = document.querySelector('.fa-bars');
+var dropdown = document.querySelector('.dropdown');
+bars.addEventListener('click', function (e) {
+  dropdown.classList.toggle('open');
+}); // Scroll to Anchor found: https://www.yogihosting.com/jquery-scroll-to-element/
+
+$('a[href^="#"]').on('click', function (e) {
+  e.preventDefault();
+  var targetEle = this.hash;
+  var $targetEle = $(targetEle);
+  dropdown.classList.remove('open');
+  $('html, body').stop().animate({
+    'scrollTop': $targetEle.offset().top
+  }, 750, 'swing', function () {
+    window.location.hash = targetEle;
+  });
+});
 
 /***/ })
 
