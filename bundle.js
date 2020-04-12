20926ed5a90bf069847069131c3ec84d19e33394
Scott Ha <123viet123@gmail.com>
2020-04-12 14:07:47 -0700

    Added fade-in/out on elements in the page

--- bundle.js
+++ bundle.js
@@ -93,6 +93,7 @@
 /*! no static exports found */
 /***/ (function(module, exports) {
 
+console.info('Thanks for visiting my website!\nFeel free to contact me with any of the links found on the navigation bar!\nI hope you enjoyed the experience.\n- Scott Ha');
 var topBtn = document.getElementById('top-btn');
 var navBarName = document.getElementById('navbar-name');
 topBtn.addEventListener('click', function (e) {
@@ -132,6 +133,29 @@ $('a[href^="#"]').on('click', function (e) {
   }, 750, 'swing', function () {
     window.location.hash = targetEle;
   });
+}); // Window Fade-In/Out found: https://stackoverflow.com/questions/26694385/fade-in-on-scroll-down-fade-out-on-scroll-up-based-on-element-position-in-win
+
+$(window).on("load", function () {
+  $(window).scroll(function () {
+    var windowBottom = $(this).scrollTop() + $(this).innerHeight();
+    $(".fade-in").each(function () {
+      /* Check the location of each desired element */
+      var objectBottom = $(this).offset().top + $(this).outerHeight();
+      /* If the element is completely within bounds of the window, fade it in */
+
+      if (objectBottom < windowBottom) {
+        //object comes into view (scrolling down)
+        if ($(this).css("opacity") == 0) {
+          $(this).fadeTo(369, 1);
+        }
+      } else {
+        //object goes out of view (scrolling up)
+        if ($(this).css("opacity") == 1) {
+          $(this).fadeTo(369, 0);
+        }
+      }
+    });
+  }).scroll(); //invoke scroll-handler on page-load
 });
 
 /***/ })
